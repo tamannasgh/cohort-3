17312ab568bb6f2b3678c3699ba8b10f47dbf03c
Tamanna Sharma <ts.harma2882006@gmail.com>
2025-02-13 13:21:49 +0530

    useMmo

--- week-11-1-customHooks-debouncing/src/App.jsx
+++ week-11-1-customHooks-debouncing/src/App.jsx
@@ -1,10 +1,20 @@
-import SearchBar from "./SearchBar";
-
+import {useState, useMemo} from "react";
 function App() {
+  const [count, setCount] = useState(0);
+  const [num, setNum] = useState(1);
+
+  const memoizedValue = useMemo(() => {
+    for(let i = 0; i < 1000000000; i++) {}
+    console.log("calculating", num);
+    return num * 2;
+  }, [num]);
+
+
   return (
     <>
-      <h1>Search bar</h1>
-      <SearchBar />
+      <button onClick={() => setCount(count + 1)}>increase {count}</button>
+      <input type="number" value={num} onChange={(e) => setNum(e.target.value)} />
+      <p>double of {num} is {memoizedValue}</p>
     </>
   )
 }
