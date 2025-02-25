640436e62ddf28e7596c7039cf013615339460a2
Tamanna Sharma <ts.harma2882006@gmail.com>
2025-02-25 13:08:09 +0530

    basic ts

--- week-14-typescript/index.js
+++ week-14-typescript/index.js
@@ -1,2 +1,12 @@
-var a = 5;
-console.log(a);
+var users = [
+    { name: 'John', age: 16 },
+    { name: 'tamanna', age: 18 },
+    { name: 'tanu', age: 45 },
+];
+console.log(users);
+function filterUsers(users) {
+    // filter method return an array, that array will contain the objects that statisfy the cndition
+    return users.filter(function (user) { return user.age >= 18; });
+}
+var filteredUsers = filterUsers(users);
+console.log(filteredUsers);
--- week-14-typescript/index.ts
+++ week-14-typescript/index.ts
@@ -1,2 +1,21 @@
-let a: number = 5;
-console.log(a);
+interface User {
+  name: string,
+  age: number
+}
+
+let users: User[] = [
+  {name: 'John', age: 16},
+  {name: 'tamanna', age: 18},
+  {name: 'tanu', age: 45},
+];
+
+console.log(users);
+
+function filterUsers(users: User[]): User[] {
+  // filter method return an array, that array will contain the objects that statisfy the cndition
+  return users.filter(user => user.age >= 18);
+}
+
+let filteredUsers = filterUsers(users);
+console.log(filteredUsers);
+
